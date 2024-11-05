de4f804ea6867df81759add65ea54d2b86ca2041
Sapein <5852983+Sapein@users.noreply.github.com>
2024-11-05 11:04:03 -0600

    Add in some changes for Associated Tags

---
+++ TSC.AvaloniaUI/Models/TagGroup.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using CSharpFunctionalExtensions;
+using DynamicData;
+using ReactiveUI;
+using TSC.AvaloniaUI.Views;
+
+namespace TSC.AvaloniaUI.Models;
+
+public class TagGroup: ReactiveObject {
+    public Tag MainTag { get; }
+    public SourceList<Tag> Tags { get;}
+
+    public TagGroup(Tag mainTag, SourceList<Tag> tags) {
+        MainTag = mainTag;
+        Tags = tags;
+    }
+
+    public Result AddTags(IEnumerable<Tag> newTags) {
+        Tags.AddRange(newTags);
+        return Result.Success();
+    }
+
+    public Result RemoveTags(IEnumerable<Tag> removedTags) {
+        Tags.RemoveMany(removedTags);
+        return Result.Success();
+    }
+}
